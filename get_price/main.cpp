1cb94b902dd4274b5b7cb6afa3837cb333a90f65
JWang925 <wangjiajun925@gmail.com>
2018-02-08 23:44:51 -0500

    modified main to run a coin-to-coin arbitrage check every 5 seconds

--- get_price/main.cpp
+++ get_price/main.cpp
@@ -3,13 +3,16 @@
 #include <iostream>
 #include "quadriga.h"
 #include "kraken.h"
+#include <fstream>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
 double EqCqBqE(); //return profitability of Ethereum (through Quadriga) to Canadian Dollar (through Quadriga) to Bitcoin to Ethereum
 double EqBqCqE(); //return profitability of Ethereum (through Quadriga)to Canadian Dollar (through Quadriga) to Bitcoin to Ethereum
 double EqBtBkE(); //return profitability of Ethereum (through Quadriga) to BTC (transfer to Kraken) to BTC (through Kraken) to Ethereum
-
+double BqEtEkB();
 int main(int argc, char* argv[])
 {
 	//cout << "margin of EqCqBqE is (without fee):" << EqCqBqE() << "%" << endl;
@@ -21,23 +24,16 @@ int main(int argc, char* argv[])
 	Quadriga q;
 	Kraken k;
 
-	cout << "ETH****************************"<<endl;
+	cout << "ETH/BTC coin-to-coin arbitrage" << endl;
+	ofstream outfile;
+	outfile.open("result.txt",std::fstream::out | std::fstream::app );
 
-	q.print_order_book(q.get_order_book("eth_cad"));
-
-	k.print_order_book(k.get_order_book("ETHCAD"));
-	k.print_order_book(k.get_order_book("ETHUSD"));
-
-
-
-	cout << "BTC****************************"<<endl;
-	q.print_order_book(q.get_order_book("btc_cad"));
-
-
-	k.print_order_book(k.get_order_book("XBTCAD"));
-	k.print_order_book(k.get_order_book("XBTUSD"));
-
-
+	while (1) {
+		outfile << std::time(0) << '\t';
+		outfile << EqBtBkE() << "%" << '\t';
+		outfile << BqEtEkB() << "%" <<endl;
+		std::this_thread::sleep_for (std::chrono::seconds(5));
+	}
 
 //	Kraken k;
 //	json::value order_book = k.get_order_book("ethcad");
@@ -105,13 +101,13 @@ double EqBtBkE(){
 
 	Quadriga q; //start an instance of q
 	json::value eth_btc_order = q.get_order_book("eth_btc"); // bid: eth -> btc; ask: btc -> eth
-	asset = asset * q.get_bid(eth_btc_order); //now asset is in bitcoin
+	asset = asset * q.get_bid(eth_btc_order); //now asset is in BTC
 	q.print_order_book(eth_btc_order);
 	cout << "asset:" <<asset << endl;
 
 	Kraken k;
 	json::value eth_xbt_order = k.get_order_book("ETHXBT");  // bid: eth -> cad; ask: cad -> eth
-	asset = asset / k.get_ask(eth_xbt_order);//now asset is in ethereum
+	asset = asset / k.get_ask(eth_xbt_order);//now asset is in ETH
 	k.print_order_book(eth_xbt_order);
 	cout << "asset:" <<asset << endl;
 
@@ -119,4 +115,26 @@ double EqBtBkE(){
 
 	return margin;
 }
+
+double BqEtEkB(){
+	double asset=100;
+
+	Quadriga q; //start an instance of q
+	json::value eth_btc_order = q.get_order_book("eth_btc"); // bid: eth -> btc; ask: btc -> eth
+	asset = asset / q.get_ask(eth_btc_order); //now asset is in ETH
+	q.print_order_book(eth_btc_order);
+	cout << "asset:" <<asset << endl;
+
+	Kraken k;
+	json::value eth_xbt_order = k.get_order_book("ETHXBT");  // bid: eth -> cad; ask: cad -> eth
+	asset = asset * k.get_bid(eth_xbt_order);//now asset is in BTC
+	k.print_order_book(eth_xbt_order);
+	cout << "asset:" <<asset << endl;
+
+	double margin = asset-100;
+
+	return margin;
+}
+
+
 
